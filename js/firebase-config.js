6435d5e0d38b90debbf2702bc8f6bbb53fd42f49
Crons0609 <christhiam.d.r.v.1590@gmail.com>
2026-04-15 21:36:59 -0600

    Conecta Firebase SDK Web con proyecto bifrost-sa real

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // Configuración del SDK Web de Firebase (Para usar directamente en la página web)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  databaseURL: "TU_FIREBASE_URL",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID_AQUI",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyASoDJQqXNWupnA0DicW6sWgm-zD7xe9HA",
+  authDomain: "bifrost-sa.firebaseapp.com",
+  databaseURL: "https://bifrost-sa-default-rtdb.firebaseio.com",
+  projectId: "bifrost-sa",
+  storageBucket: "bifrost-sa.firebasestorage.app",
+  messagingSenderId: "943196918782",
+  appId: "1:943196918782:web:5e197c094948a6517a66af"
 };
 
 // Importamos Firebase desde los CDN oficiales y lo inicializamos
@@ -16,3 +16,4 @@ import { getDatabase, ref, onValue, set, push, update } from 'https://www.gstati
 const app = initializeApp(firebaseConfig);
 export const dbWeb = getDatabase(app);
 export { ref, onValue, set, push, update };
+
